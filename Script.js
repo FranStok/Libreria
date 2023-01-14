a404660c39e88bc7b4fc3b9b4dd282dd6c85f0d7
Francico <franciscoastoreca@hotmail.com>
2023-01-13 22:50:45 -0300

    Commit

--- Script.js
+++ Script.js
@@ -1,2 +1,21 @@
-/*grid-template-columns: repeat(auto-fit,minmax(275px,1fr));
-#e2e8f0;*/
+/*Cambiar el estado de un libro leido a no leido y al reves */
+const Leido=document.querySelectorAll(".Leido");
+Leido.forEach(Libro => {
+    Libro.addEventListener("click",(e)=>{
+        if(Libro.textContent==="No leído"){
+            Libro.textContent="Leído"
+            Libro.style.backgroundColor="#00FF00";
+        }else{
+            Libro.textContent="No leído"
+            Libro.style.backgroundColor="#a9aeb4";
+        }
+    })
+});
+/*Eliminar el libro cada vez que se apreta el boton de eliminar*/
+const Eliminar=document.querySelectorAll(".Eliminar");
+Eliminar.forEach(Libro => {
+    Libro.addEventListener("click",(e)=>{
+        Libro.parentElement.remove();
+    })
+});
+
