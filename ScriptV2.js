b8f813f79c1e20cd2efc89c5c4124df4e65ac929
Francico <franciscoastoreca@hotmail.com>
2023-01-16 15:57:16 -0300

    Commit

--- ScriptV2.js
+++ ScriptV2.js
@@ -1,67 +1,3 @@
-/*Muestra el formulario cuando se presiona agregar libro*/
-const Agregar=document.querySelector(".Agregar");
-Agregar.addEventListener("click",(e)=>{
-    let form=document.querySelector("form")
-    Agregar.style.display="none";
-    form.style.display="block"
-})
-/*
-const Submit=document.querySelector(".Submit");
-Submit.addEventListener("click",(event)=>{
-    event.preventDefault();
-    let Titulo=document.querySelector("#Titulo"),
-    Autor=document.querySelector("#Autor"),
-    Paginas=document.querySelector("#Paginas");
-    let Libro=new Libros(Titulo.value,Autor.value,Paginas.value,"No leído")
-    /*Creo todos los elementos de un libro
-    if(TextoAutor!="" && TextoTitulo!="" && TextoPaginas!=""){
-        let Libros=document.querySelector(".Libros"),
-        Libro=document.createElement("div"),
-        Ptitulo=document.createElement("p"),
-        Pautor=document.createElement("p"),
-        Ppaginas=document.createElement("p"),
-        Leido=document.createElement("Button"),
-        Eliminar=document.createElement("Button");
-        Libros.classList.add("Libros");
-        Libro.classList.add("Libro");
-        Ptitulo.textContent=TextoTitulo;
-        Pautor.textContent=TextoAutor;
-        Ppaginas.textContent=TextoPaginas;
-        Leido.textContent="No leído";
-        Leido.classList.add("Leido");
-        Eliminar.textContent="Eliminar libro";
-        Eliminar.classList.add("Eliminar");
-        /*Agrego todos los elementos a sus padres
-        Libros.appendChild(Libro);
-        Libro.appendChild(Ptitulo);
-        Libro.appendChild(Pautor);
-        Libro.appendChild(Ppaginas);
-        Libro.appendChild(Leido);
-        Libro.appendChild(Eliminar);
-        /*Agrego los eventListeners que se usaron al principio, por lo tanto estos no los tienen
-        Leido.addEventListener("click",(e)=>{
-            if(Leido.textContent==="No leído"){
-                Leido.textContent="Leído"
-                Leido.style.backgroundColor="#00FF00";
-            }else{
-                Leido.textContent="No leído"
-                Leido.style.backgroundColor="#a9aeb4";
-            }
-        })
-        Eliminar.addEventListener("click",(e)=>{
-            Eliminar.parentElement.remove();
-        })
-        let form=document.querySelector("form");
-        form.style.display="none";
-        let Agregar=document.querySelector(".Agregar");
-        Agregar.style.display="inline"
-        /*Vacio los inputs
-        Autor.value="";
-        Titulo.value="";
-        Paginas.value="";
-    }
-})*/
-
 let Libreria=[]
 
 function Libro(Titulo,Autor,Paginas,Leido){
@@ -88,21 +24,37 @@ function Libro(Titulo,Autor,Paginas,Leido){
         Eliminar.classList.add("Eliminar");
         /*Agrego todos los elementos a sus padres*/
         Libros.appendChild(Libro);
-        Libro.appendChild(autor);
         Libro.appendChild(titulo);
+        Libro.appendChild(autor);
         Libro.appendChild(paginas);
         Libro.appendChild(Leido);
         Libro.appendChild(Eliminar);
+        CambiarLeido(Leido);
+        EliminarLibro(Eliminar);
     }
 }
-
-function NuevoLibro(Titulo,Autor,Paginas){
-    Libreria[Libreria.length]=new Libro(Titulo,Autor,Paginas)
-    Libreria[Libreria.length-1].AgregarLibro();
+function NoExiste(Titulo){
+    let I=0;
+    while(I<Libreria.length && Libreria[I].Titulo!=Titulo)
+        I++;
+    if(I==Libreria.length)
+        return true;
+    else
+        return false;
 }
-NuevoLibro("H","O","1");
-NuevoLibro("L","A","2");
-NuevoLibro("C","H","3");
+function BuscarLibro(Titulo){
+    let I=0;
+    while(I<Libreria.length && Libreria[I].Titulo!=Titulo)
+        I++;
+    return I;
+}
+function NuevoLibro(Titulo,Autor,Paginas){
+    if(NoExiste(Titulo)){
+        Libreria[Libreria.length]=new Libro(Titulo,Autor,Paginas)
+        Libreria[Libreria.length-1].AgregarLibro();
+    }else
+        alert("El libro que queres ingresar ya esta en la libreria.")
+}
 
 const Submit=document.querySelector(".Submit");
 Submit.addEventListener("click",(event)=>{
@@ -111,4 +63,43 @@ Submit.addEventListener("click",(event)=>{
     Autor=document.querySelector("#Autor"),
     Paginas=document.querySelector("#Paginas");
     NuevoLibro(Titulo.value,Autor.value,Paginas.value);
+    let form=document.querySelector("form");
+    form.style.display="none";
+    let Agregar=document.querySelector(".Agregar");
+    Agregar.style.display="inline"
+    /*Vacio los inputs*/
+    Autor.value="";
+    Titulo.value="";
+    Paginas.value="";
 })
+
+/*Muestra el formulario cuando se presiona agregar libro*/
+const Agregar=document.querySelector(".Agregar");
+Agregar.addEventListener("click",(e)=>{
+    let form=document.querySelector("form")
+    Agregar.style.display="none";
+    form.style.display="block"
+})
+
+
+/*Cambiar el estado de un libro leido a no leido y al reves */
+function CambiarLeido(Libro){
+    Libro.addEventListener("click",(e)=>{
+        if(Libro.textContent==="No leído"){
+            Libro.textContent="Leído"
+            Libro.style.backgroundColor="#00FF00";
+        }else{
+            Libro.textContent="No leído"
+            Libro.style.backgroundColor="#a9aeb4";
+        }
+    })
+}
+/*Eliminar el libro cada vez que se apreta el boton de eliminar*/
+function EliminarLibro(Eliminar){
+    Eliminar.addEventListener("click",(e)=>{
+        /*Busco el titulo del libro*/
+        let Titulo=Eliminar.parentElement.firstChild.nextSibling.textContent;
+        Eliminar.parentElement.remove();
+        Libreria.splice(BuscarLibro(Titulo),1)
+    });
+}
